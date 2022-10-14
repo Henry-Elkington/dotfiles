428f09a96bf4b874dc41a6995a89c3a8b353be6b
Henry-Elkington <henry.d.elkington@gmail.com>
2022-10-14 12:41:12 -0600

    organise

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/bin/env sh
+# stow -d $PWD/Files -t $HOME -S nvim
 
-stow -d $PWD/Files -t $HOME -S nvim
+# rm -rf $HOME/.cache $HOME/.local/share/nvim $HOME/.local/state/nvim $HOME/dotfiles/Files/nvim/.config/nvim/plugin
 
+# stow -D -d $PWD/Files -t $HOME -S nvim
--- remove.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env sh
-
-rm -rf $HOME/.cache $HOME/.local/share/nvim $HOME/.local/state/nvim $HOME/dotfiles/Files/nvim/.config/nvim/plugin
-stow -D -d $PWD/Files -t $HOME -S nvim
-
